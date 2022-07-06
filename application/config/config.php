4639917e5392326ec3eac84856f2146401eadaa6
ryzen500 <ahmadtsani145@gmail.com>
2022-07-06 11:37:36 +0700

    htaccess add

--- application/config/config.php
+++ application/config/config.php
@@ -14,9 +14,9 @@
 | path to your installation.
 |
 */
-// $config['base_url'] = "";
+ $config['base_url'] = "";
 
-$config['base_url'] = "10.255.158.64:8084";
+//$config['base_url'] = "10.255.158.64:8084";
 
 //$config['server_root']=$_SERVER['DOCUMENT_ROOT'];
 
